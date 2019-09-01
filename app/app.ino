9d55c6fed104e5f8ac21b9e231c3a96a1f572456
Omer <oyesilo@yahoo.ca>
2019-09-01 15:42:05 -0400

    working sample

---
+++ app/app.ino
@@ -0,0 +1,49 @@
+#include "mjson.h"  // Sketch -> Add File -> Add mjson.h
+
+static int ledOn = 0;  // Current LED status
+static long serialBaundRate = 115200;
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+
+
+static void reportState(void) {
+  jsonrpc_call("{\"method\":\"Shadow.Report\",\"params\":{\"on\":%s}}",
+               ledOn ? "true" : "false");
+}
+
+void setup() {
+  // Init RPC library. Pass a "sender" function that writes RPC reply frame.
+  jsonrpc_init([](const char *frame, int frame_len, void *privdata) {
+    return (int) Serial.write(frame, frame_len);
+  }, NULL, NULL, "1.0");
+
+  // Export "Shadow.Delta". Pass a callback that updates ledOn
+  jsonrpc_export("Shadow.Delta", [](struct jsonrpc_request * r) {
+    ledOn = mjson_get_bool(r->params, r->params_len, "$.on", 0);
+    digitalWrite(LED_BUILTIN, ledOn); // Set LED to the "on" value
+    reportState();                    // Let shadow know our new state
+    jsonrpc_return_success(r, NULL);  // Report success
+  }, NULL);
+
+  pinMode(LED_BUILTIN, OUTPUT);   // Configure LED pin
+  Serial.begin(serialBaundRate);           // Init serial comms
+  reportState();                  // Let shadow know our state
+
+  pinMode(pushButton, INPUT);
+}
+
+// static int uptime = 0;   // Time after last reboot in seconds
+void loop() {
+  if (Serial.available() > 0) jsonrpc_process_byte(Serial.read());
+  // if (millis() / 1000 > uptime) {
+  //   uptime = millis() / 1000;
+  //   jsonrpc_call("{\"method\":\"Shadow.Report\",\"params\":{\"uptime\":%d}}", uptime);
+  // }
+
+  // read the input pin:
+  // int buttonState = digitalRead(pushButton);
+  // Serial.println(buttonState);
+  // delay(2000);
+
+}
